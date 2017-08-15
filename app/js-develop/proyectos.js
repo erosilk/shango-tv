246de1c30980c5084f97bcf256f7798a8c4723c9
Micael Robles <micael.robles@gmail.com>
2017-08-15 16:01:13 -0300

    pagina proyectos

--- app/js-develop/proyectos.js
+++ app/js-develop/proyectos.js
@@ -8,20 +8,62 @@ function doResizing() {
     })
   }, 100)
 }
+
 function squarify() {
   var width = $('.thumb').css('width')
   $('.thumb').css('height', width)
 }
 
-$(document).ready(function() {
-  setTimeout(function() {
-    doResizing()
-    squarify()
-  }, 200)
-})
+if ($('#home-proyectos')[0]) {
+  $(document).ready(function() {
+    setTimeout(function() {
+      doResizing()
+      squarify()
+    }, 200)
+  })
+  var windowResize = _.debounce(doResizing, 200)
 
-var windowResize = _.debounce(doResizing, 200)
+  $(window).resize(windowResize)
 
-$(window).resize(windowResize)
+  $(window).resize(squarify)
+}
 
-$(window).resize(squarify)
+if ($('#proyectos')[0]) {
+  function squarify() {
+    var width = $('.thumb').css('width')
+    $('.thumb').css('height', width)
+  }
+  $(document).ready(function() {
+    setTimeout(function() {
+      doResizing()
+      squarify()
+    }, 200)
+  })
+
+  $(window).resize(squarify)
+
+  $('.proyectos-list').isotope({
+    // options
+    itemSelector: '.proyecto',
+    layoutMode: 'fitRows',
+    transitionDuration: 0,
+  })
+
+  var $grid = $('.proyectos-list')
+
+  function filter(tag) {
+    $grid.isotope({ filter: tag })
+  }
+
+  $('.tags ul li').on('click', function() {
+    if (!$(this).hasClass('selected')) {
+      var tag = $(this).attr('class')
+      filter('.' + tag)
+      if (tag === 'todos') {
+        filter('')
+      }
+      $('.tags ul li.selected').removeClass('selected')
+      $(this).addClass('selected')
+    }
+  })
+}
